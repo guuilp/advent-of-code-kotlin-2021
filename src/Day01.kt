932c16ef0e58cf3c3d64294ab73ae48e0dbf7d85
Guilherme Lima Pereira <guuuilp@gmail.com>
2021-12-04 17:09:01 -0300

    Refactoring Day01 to respect scaffold

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,31 +1,31 @@
 fun main() {
+    fun part1(input: List<Int>): Int {
+        var measurementsLargerThanPrevious = 0
+
+        input.forEachIndexed { index, i ->
+            if (index != 0 && i > input[index - 1]) measurementsLargerThanPrevious++
+        }
+
+        return measurementsLargerThanPrevious
+    }
+
+    fun part2(input: List<Int>): Int {
+        var measurementsLargerThanPrevious = 0
+
+        (0..input.size - 4).forEach { index ->
+            val currentTree = input[index] + input[index + 1] + input[index + 2]
+            val nextTree = input[index + 1] + input[index + 2] + input[index + 3]
+
+            if (nextTree > currentTree) measurementsLargerThanPrevious++
+        }
+
+        return measurementsLargerThanPrevious
+    }
+
     val inputAsStringList: List<String> = readInput("Day01_test")
     val inputAsIntList = inputAsStringList.map { it.toInt() }
 
     println("Part 1 result: ${part1(inputAsIntList)}")
     println("Part 2 result: ${part2(inputAsIntList)}")
 
-}
-
-fun part1(input: List<Int>): Int {
-    var measurementsLargerThanPrevious = 0
-
-    input.forEachIndexed { index, i ->
-        if (index != 0 && i > input[index - 1]) measurementsLargerThanPrevious++
-    }
-
-    return measurementsLargerThanPrevious
-}
-
-fun part2(input: List<Int>): Int {
-    var measurementsLargerThanPrevious = 0
-
-    (0..input.size - 4).forEach { index ->
-        val currentTree = input[index] + input[index + 1] + input[index + 2]
-        val nextTree = input[index + 1] + input[index + 2] + input[index + 3]
-
-        if (nextTree > currentTree) measurementsLargerThanPrevious++
-    }
-
-    return measurementsLargerThanPrevious
 }
